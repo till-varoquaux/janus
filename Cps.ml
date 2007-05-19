60a0c846e7f7d9007ca15535dbf281f20bfe3bae
till.varoquaux <till.varoquaux@gmail.com>
2007-05-19 14:15:31 +0000

    Cps pass doesn't do hoisting anymore...

--- Cps.ml
+++ Cps.ml
@@ -1,93 +1,58 @@
 open AstCpsInt
 
-type env={
- cont:AstJs.instr list;
- head:AstJs.instr list;
- vdecl:AstJs.ident list
-}
-
-let emptyEnv={
- cont=[];
- head=[];
- vdecl=[]
-}
-
 let return="$return"
 
 let cpsCall inf affect=
- let a= (match affect with
+ let args= (match affect with
           | None -> []
           | Some i -> [i])
- and cont=Env.fresh ~hint:"CpsCall" () in
- {inf with
-   vdecl=cont::inf.vdecl;
-   head=`Fundecl (cont,a,`Bloc (inf.cont))::inf.head;
- },cont
+ and fname=Env.fresh ~hint:"CpsCont" () in
+ [(`Vdecl [fname]);(`Fundecl (fname,args,`Bloc inf))],(`Ident fname)
 
 let rec instr (i:AstCpsInt.instr) inf=
  match i with
   | `Bloc b -> bloc b inf
   | `CpsTemplateCall (el,b) ->
-     let inf,cont=cpsCall inf None in
-     {inf with
-       cont=[`TemplateCall (`Lval (`Ident cont)::el,b)]
-     }
+     let call,cont=cpsCall inf None in
+     call@[`TemplateCall (`Lval cont::el,b)]
   | `CpsCall (a,e,el) ->
-     let inf,cont=cpsCall inf a in
-     {inf with
-       cont=[`Call (e,`Lval (`Ident cont)::el)]
-     }
+     let call,cont=cpsCall inf a in
+     call@[`Call (e,`Lval cont::el)]
   | `Cpsdecl (i,al,b) ->
-     {inf with
-       head=`Fundecl (i,(return::al),fbloc b)::inf.head;
-       vdecl=i::inf.vdecl
-     }
+     (`Fundecl (i,(return::al),fbloc b))::inf;
   | `Fundecl (i,al,b) ->
-     {inf with
-       head=`Fundecl(i,al,fbloc b)::inf.head;
-       vdecl=i::inf.vdecl
-     }
-  | `Vardecl (i,e) ->
-     {inf with
-       cont=(`Assign((`Ident i),e))::inf.cont;
-       vdecl=i::inf.vdecl
-     }
+     (`Fundecl(i,al,fbloc b))::inf;
       (*optimize me this...*)
   | `If (e,b1,b2) (*when (Env.cps inf.env)*)  ->
      let k=Env.fresh ~hint:"Ite" () in
-     let inf={(*inf with*)
-      head=(`Fundecl(k,[],`Bloc (inf.cont)))::inf.head;
-               cont=([`Call(`Lval(`Ident k),[])]);
-               vdecl=k::inf.vdecl
-             }
+     let inf=[`Vdecl [k];
+              `Fundecl(k,[],`Bloc inf);
+              `Call(`Lval(`Ident k),[])]
      in
      let e1=instr b1 inf in
-     let e2=instr b2 {e1 with cont=inf.cont} in
-     {e2 with cont=[`If (e,(`Bloc e1.cont),(`Bloc e2.cont))]}
+     let e2=instr b2 inf in
+     [`If (e,`Bloc e1,`Bloc e2)]
   | `While (e,i) (*when (Env.cps inf.env)*) ->
      let k=Env.fresh ~hint:"While" () in
      let cont=[`Call ((`Lval(`Ident k)),[])] in
-     let i=instr i {inf with cont=cont} in
-     {(*i with*)
-      vdecl=k::i.vdecl;
-      head=(`Fundecl (k,[],`Bloc [`If(e,(`Bloc i.cont),(`Bloc inf.cont))]))::i.head;
-      cont=cont
-     }
+     let i=instr i cont in
+     [`Vdecl [k];
+      `Fundecl (k,[],`Bloc [`If(e,(`Bloc i),(`Bloc inf))]);
+      `Call (`Lval(`Ident k),[])]
   | `CpsRet e ->
-     {inf with cont=(`Call ((`Lval(`Ident return)),[e]))::inf.cont}
-  | `Assign _ | `Call _ | `Vdecl _ | `Ret _ | `TemplateCall _ as i ->
-     {inf with cont=(i:>AstJs.instr)::inf.cont}
+     (`Call ((`Lval(`Ident return)),[e]))::inf
+  | `Vardecl _ | `Assign _ | `Call _ | `Vdecl _ | `Ret _ | `TemplateCall _ as i ->
+     (i:>AstJs.instr)::inf
 
 and fbloc b =
- let c=instr b emptyEnv in
- `Bloc ((`Vdecl c.vdecl)::(c.head)@(c.cont))
+ let c=instr b [] in
+ `Bloc c
 
-and bloc b (cpsInfo:env) : env=
+and bloc b cpsInfo =
  List.fold_right instr b cpsInfo
 
 and program p=
- let c=bloc p emptyEnv in
- (`Vdecl c.vdecl)::(c.head)@(c.cont)
+ bloc p []
 
 let run=
  program
