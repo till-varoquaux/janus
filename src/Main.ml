0b998a6a73d1bcc7863c6db7a476bcad9d5733e5
till.varoquaux <till.varoquaux@gmail.com>
2007-08-29 23:55:01 +0000

    Added the ability to read from stdin

--- src/Main.ml
+++ src/Main.ml
@@ -3,14 +3,13 @@
   This is the entry point of the programm it reads command line options and
   contains the chain of all of the compiler passes.
 *)
-open Format
-open Lexing
-open Lexer
-open Parser
 open General
 
 let usage = Printf.sprintf "usage: %s [options] file" (Filename.basename
                                                         Sys.executable_name)
+
+let readFromStdIn=ref false
+
 let version ()=
  Printf.printf "%s, version: \"%s\"\n" Version.name Version.version;
  exit 0
@@ -25,37 +24,35 @@ let (++) f g x= g (f x)
 
 
 let parseError lexbuf s=
- eprintf "%s\n%s\n@." (Pos.locToString (lexeme_start_p lexbuf,lexeme_end_p lexbuf)) s;
+ Printf.eprintf "%s\n%s\n@." (Pos.locToString
+                               (Lexing.lexeme_start_p lexbuf,
+                                Lexing.lexeme_end_p lexbuf)) s;
  exit 1
 
-let parse file=
- with_open_in file begin
-  fun c ->
-   let lb = Lexing.from_channel c in
-   try
-    Lexer.setFile lb file;
-    Parser.program Lexer.token lb
-   with
-    | Lexical_error s ->
-       parseError lb (sprintf "lexical error: %s" s)
-    | Parsing.Parse_error ->
-       parseError lb (sprintf "syntax error in parse rule: \"%s\"" !ParseInfo.currentRule);
- end
-
-let process=
- parse++Cps.compile
+let parse fname chan=
+ let lb = Lexing.from_channel chan in
+ try
+  Lexer.setFile lb fname;
+  Parser.program Lexer.token lb
+ with
+  | Lexer.Lexical_error s ->
+     parseError lb (Printf.sprintf "lexical error: %s" s)
+  | Parsing.Parse_error ->
+     parseError lb (Printf.sprintf "syntax error in parse rule: \"%s\""
+                     !ParseInfo.currentRule)
 
 let specs=Arg.align (Cps.specs@specs)
 
 let () =
  let file = ref None in
  Arg.parse specs (fun s -> file := Some s) usage;
- match !file with
-  |Some f ->begin
-     try
-      process f
-     with e ->
-	  eprintf "Anomaly: %s\n@." (Printexc.to_string e);
-	  exit 2
-    end
-  | None -> Arg.usage specs usage; exit 1
+ try
+  let ast=match !file,!readFromStdIn with
+   |Some f,false ->
+     with_open_in f (parse f)
+   | None,true -> parse "<STDIN>" stdin
+   | _ -> Arg.usage specs usage; exit 1
+  in Cps.compile ast
+ with e ->
+  Printf.eprintf "Anomaly: %s\n@." (Printexc.to_string e);
+  exit 2
