b22f9ad3b330453d66b47924944d61c73206b544
till.varoquaux <till.varoquaux@gmail.com>
2007-09-07 21:28:39 +0000

    Moved TailRet to a real usefull monad

--- src/TailRet.ml
+++ src/TailRet.ml
@@ -4,66 +4,66 @@
  * functions call return implicitly when they end we might as well remove them.
  *)
 
-module T=AstJs.Trav.Map(Monad.Id)
-
-module D=T.Make(
- functor(S:T.Translation) ->
+type pos = LoopTail | FunTail | Normal
+module EndMon=
  struct
-  module Super=T.Base(S)
+  type 'a m = pos -> string option -> 'a
+  let return m = fun _ _ -> m
+  let bind v f = fun _ lbl -> f (v Normal lbl) Normal lbl
+  let run v = v Normal None
+ end
+
+module Mapper=AstJs.Trav.Map(EndMon)
+module D'=Mapper.Make(
+ functor(S:Mapper.Translation) ->
+ struct
+  module Super=Mapper.Base(S)
   include Super
 
-  let rec trimEndInstr lbl inFun=
-   let self i=trimEndInstr lbl inFun i in
-   let rec trimBloc=function
-    | [] -> []
-    | [i] -> [self i]
-    | h::t-> (S.instr h)::(trimBloc t)
-   in
-   function
+  let rec bloc b pos loopName =
+   match b with
+   | [] -> []
+   | h::t ->
+      let cont=bloc t pos loopName in
+      let hPos= match cont with [] -> pos | _ -> Normal
+      in match S.instr h hPos loopName with
+       | `Bloc [] -> cont
+       | i->i::cont
+
+  let instr i pos loopName =
+   match i with
     | `Fundecl(name,args,body) ->
-       let body=trimEndInstr None true body in
-       `Fundecl(name,args,body)
-    | `Ret None when inFun -> `Bloc []
-    | `Bloc b -> `Bloc (trimBloc b)
+       `Fundecl(name,args,S.instr body FunTail None)
+    | `Ret None when pos=FunTail -> `Bloc []
+    | `Continue i when (pos=LoopTail) && (loopName=Some i) -> `Bloc []
     | `If (e,i1,i2) ->
-       let e'=S.expr e
-       and i1'=self i1
-       and i2'=self i2
-       in
-       `If(e',i1',i2')
+       let e'=S.expr e Normal loopName
+       and i1'=S.instr i1 pos loopName
+       and i2'=S.instr i2 pos loopName
+       in `If(e',i1',i2')
     | `WithCtx(e,i,ids) ->
-       let e'=S.expr e
-       and i'=self i in
+       let e'=S.expr e Normal loopName
+       and i'=S.instr i pos loopName in
        `WithCtx(e',i',ids)
     | `Labeled(lbl,`While(e,i)) ->
-       let e'=S.expr e
-       and i'=trimEndInstr (Some lbl) false i in
+       let e'=S.expr e Normal (Some lbl)
+       and i'=S.instr i LoopTail (Some lbl) in
        `Labeled(lbl,`While(e',i'))
-    | `Continue l when lbl=Some l -> `Bloc []
-    | `Labeled(lbl,i) -> `Labeled(lbl, self i)
-    | i -> S.instr i
+    | `Labeled(lbl,i) ->
+       `Labeled(lbl, S.instr i pos loopName)
+    | `Bloc b -> `Bloc (bloc b pos loopName)
+    | i -> Super.instr i pos loopName
 
-  let instr = function
-   | `Fundecl(name,args,body) ->
-      let body=trimEndInstr None true body in
-      `Fundecl(name,args,body)
-   | `Labeled(lbl,`While(e,i)) ->
-      let e'=S.expr e
-      and i'=trimEndInstr (Some lbl) false i in
-      `Labeled(lbl,`While(e',i'))
-   | i -> Super.instr i
-
-  let expr = function
-   | `Fun (args,body) ->
-      let body'=trimEndInstr None true body in
-      `Fun(args,body')
-   | e -> Super.expr e
- end
-)
+  let expr e pos loopName =
+    match e with
+     | `Fun (args,body) ->
+        `Fun(args,S.instr body FunTail None)
+     | e -> Super.expr e pos loopName
+ end)
 
 let pass:#Optimise.pass=
 object
- method run = D.program
+ method run p = EndMon.run (D'.program p)
  method name="tailret"
  method description="Remove all the trailling empty \"return\" statements."
 end
